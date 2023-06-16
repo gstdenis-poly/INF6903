391a4ea51f9524dbb0062b7a7a971a593f535aad
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-06-16 13:03:53 -0400

    Extractor working with .final file

--- server/extractor.py
+++ server/extractor.py
@@ -9,9 +9,13 @@ import sys
 uploads_folder = '/home/gstdenis/scratch/uploads/'
 frames_folder = '/home/gstdenis/scratch/frames/'
 database_folder = '/home/gstdenis/projects/def-gabilode/gstdenis/database/'
+recording_files = [
+    'mouse_recording.txt', 'keyboard_recording.txt', 'screen_recording.mp4'
+    ]
+workers_count = 5
 
 # Extract frames from video
-def extract_frames(video_name, workers_count):
+def extract_frames(video_name):
     cap = cv2.VideoCapture(uploads_folder + video_name)
 
     video_name = os.path.splitext(video_name)[0]
@@ -33,11 +37,29 @@ def extract_frames(video_name, workers_count):
     cap.release()
     cv2.destroyAllWindows()
 
+# Extract recording files from uploads to database and extract frames if video
+def extract_file(file_name, src_folder, dest_folder):
+    file_path = src_folder + file_name
+    if not os.path.isfile(file_path):
+        return
+    
+    shutil.move(file_path, dest_folder + file_path)
+
+    # Extract frames of video 
+    if os.path.splitext(file_name)[1] == '.mp4':
+        extract_frames(file_name)
+
+    print('Extraction of file ' + file_name + ' completed')
+
 # Program main function
-def extract(workers_count):
+def extract():
     for file_name in os.listdir(uploads_folder):
         file_path = uploads_folder + file_name
         if not os.path.isfile(file_path):
+            continue
+
+        # Skip iteration if not .final file
+        if os.path.splitext(file_name)[1] != '.final':
             continue
 
         # Save file into database
@@ -45,33 +67,27 @@ def extract(workers_count):
         rec_db_folder = database_folder + recording_id + '/'
         if not os.path.exists(rec_db_folder):
             os.mkdir(rec_db_folder)
-        shutil.copyfile(file_path, rec_db_folder + file_name)
+        for rec_file in recording_files:
+            extract_file(recording_id + '_' + rec_file, uploads_folder, rec_db_folder)
 
-        # Create frames worker folder for each detector server
-        for i in range(workers_count):
-            frames_worker_folder = frames_folder + 'worker' + str(i + 1)
-            if os.path.exists(frames_worker_folder):
-                continue
-                
-            os.mkdir(frames_worker_folder)
-
-        # Extract frames of video 
-        file_ext = os.path.splitext(file_name)[1]
-        if file_ext == '.mp4':
-            extract_frames(file_name, workers_count)
-
-        # Delete file after save into database and extraction
+        # Delete .final file after storage into database and extraction
         os.remove(file_path)
 
         # Print completion of file extraction
-        print('Extraction of file ' + file_name + ' completed')
+        print('Extraction of recording ' + recording_id + ' completed')
 
 # Program's main
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Wrong arguments')
-    else:
-        workers_count = int(sys.argv[1])
-        while True:
-            extract(workers_count)
+    workers_count = int(sys.argv[1])
+
+    # Create frames worker folder for each detector server
+    for i in range(workers_count):
+        frames_worker_folder = frames_folder + 'worker' + str(i + 1)
+        if os.path.exists(frames_worker_folder):
+            continue
+            
+        os.mkdir(frames_worker_folder)
+
+    while True:
+        extract()
 
