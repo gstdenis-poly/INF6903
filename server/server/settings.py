b6b53bb88b69503e99ababa840a5514f7c3ecbdd
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-18 16:45:09 -0400

    Debug static file settings #1

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from workers.configurator import database_folder
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    database_folder,
+    '/home/gstdenis/projects/def-gabilode/gstdenis/database/',
 ]
 
 
