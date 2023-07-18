29e287743a5d30e7e66d8595141d8e7bf9187626
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-18 15:49:25 -0400

    un/register, view/edit_account & upload_recording

--- server/web/views.py
+++ server/web/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+import os
+import shutil
+from workers.configurator import logos_folder, uploads_folder
 
 # Create your views here.
 def index(request):
@@ -37,53 +40,89 @@ def register(request):
                           password = make_password(request.POST['password1']),
                           type = request.POST['type'],
                           company = request.POST['company'],
-                          summary = request.POST['summary'],
-                          logo = request.POST['logo'])
+                          summary = request.POST['summary'])
+        logo_img = request.FILES['logo']
+        if logo_img != None:
+            logo_img_ext = os.path.splitext(request.FILES['logo'].name)[-1]
+            db_logo_img_path = logos_folder + account.username + logo_img_ext
+            with open(db_logo_img_path, 'wb+') as db_logo_img:
+                for c in logo_img.chunk():
+                    db_logo_img.write(c)
+            account.logo = db_logo_img_path
         account.save()
 
         return render(request, 'logged_out/register.html')
 
-"""def unregister(request, user_id):
+def unregister(request):
     if request.user.is_authenticated:
-        # TODO
-    else:
-        # TODO
+        account = Account.objects.get(username = request.user.username)
+        account.delete()
 
-    return HttpResponse('Unregister %s.' % user_id)
+    return render(request, 'logged_out/log_in.html')
 
 def view_account(request, user_id):
     if request.user.is_authenticated:
-        # TODO
+        account = Account.objects.get(username = request.user.username)
+        return render(request, 'logged_in/view_account.html', {'account' : account})
     else:
-        # TODO
-
-    return HttpResponse('View account %s.' % user_id)
+        return render(request, 'logged_out/index.html')
 
 def edit_account(request, user_id):
+    account = Account.objects.get(username = request.user.username)
+    if request.user.is_authenticated:
+        if user_id == account.username:
+            if 'username' in request.POST:
+                account.email = request.POST['email']
+                account.password = make_password(request.POST['password1'])
+                account.company = request.POST['company']
+                account.summary = request.POST['summary']
+                if 'logo' in request.FILES:
+                    logo_img = request.FILES['logo']
+                    logo_img_ext = os.path.splitext(logo_img.name)[-1]
+                    db_logo_img_path = logos_folder + account.username + logo_img_ext
+                    with open(db_logo_img_path, 'wb+') as db_logo_img:
+                        for c in logo_img.chunk():
+                            db_logo_img.write(c)
+                    account.logo = db_logo_img_path
+                account.save()
+                
+                return render(request, 'logged_in/edit_account.html', {'account' : account})
+            else:
+                return render(request, 'logged_in/edit_account.html', {'account' : account}) 
+        else:
+            return render(request, 'logged_in/view_account.html', {'account' : account})
+    else:
+        return render(request, 'logged_out/index.html')
+
+"""def download_client(request):
     if request.user.is_authenticated:
         # TODO
     else:
         # TODO
 
-    return HttpResponse('Edit account %s.' % user_id)
-
-def download_client(request):
-    if request.user.is_authenticated:
-        # TODO
-    else:
-        # TODO
-
-    return HttpResponse('Download client')
+    return HttpResponse('Download client')"""
 
 def upload_recording(request):
     if request.user.is_authenticated:
-        # TODO
+        if request.FILES:
+            for file in request.FILES:
+                upload_file_path = uploads_folder + file.name
+                with open(upload_file_path, 'wb+') as upload_file:
+                    for c in file.chunk():
+                        upload_file.write(c)
+            
+                upload_folder_name = uploads_folder + os.path.splitext(file.name)[0]
+                shutil.unpack_archive(upload_file_path, upload_folder_name, 'zip')
+                os.remove(upload_file_path) # Remove .zip file after uncompressing it
+                open(upload_folder_name + '.final', 'w').close() # .final file for worker notif
+
+            return render(request, 'logged_in/upload_recording.html')
+        else:
+            return render(request, 'logged_in/upload_recording.html')
     else:
-        # TODO
+        return render(request, 'logged_out/index.html')
 
-    return HttpResponse('Upload recording')
-
-def view_request(request, request_id):
+"""def view_request(request, request_id):
     if request.user.is_authenticated:
         # TODO
     else:
