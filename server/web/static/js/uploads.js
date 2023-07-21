6a4d1a4cc4f8928d6934dde018f39ce3f49bb3a0
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-21 18:28:02 -0400

    Add event listeners to request management buttons

---
+++ server/web/static/js/uploads.js
@@ -0,0 +1,30 @@
+(function () {
+    "use strict";
+  
+    let btnFilterRecordings = document.querySelectorAll('.filter-recordings');
+    btnFilterRecordings.forEach( function(e) {
+        e.addEventListener('click', function(event) {
+            let btnsManageRequest = document.querySelectorAll('.btn-view-request, ' +
+                                                              '.btn-delete-request');
+            for (const btn of btnsManageRequest) {
+                btn.classList.add('btn-request-hidden');
+                btn.href = '/' + btn.href.split('/')[1] + '/'
+            }
+        });
+    });
+
+    let btnFilterRequest = document.querySelectorAll('.filter-request');
+    btnFilterRequest.forEach( function(e) {
+        e.addEventListener('click', function(event) {
+            request_id = event.target.getAttribute('data-filter').replace('.filter-request', '');
+            let btnsManageRequest = document.querySelectorAll('.btn-view-request, ' +
+                                                              '.btn-delete-request');
+            for (const btn of btnsManageRequest) {
+                btn.classList.remove('btn-request-hidden');
+                btn.href = '/' + btn.href.split('/')[1] + '/' + request_id + '/'
+            }
+        });
+    });
+    
+  })();
+  
