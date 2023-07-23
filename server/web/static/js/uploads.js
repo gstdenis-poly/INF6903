8ea36828e303b4cdfd25f11745175d18408c24b1
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-23 13:37:35 -0400

    Display create-request button when cell selected

--- server/web/static/js/uploads.js
+++ server/web/static/js/uploads.js
@@ -2,10 +2,14 @@
     "use strict";
   
     let btnFilterRecordings = document.querySelectorAll('.filter-recordings');
+    let btnFilterRequest = document.querySelectorAll('.filter-request');
+    let btnsManageRequest = document.querySelectorAll('.btn-view-request, ' +
+                                                      '.btn-delete-request');
+    let btnCreateRequest = document.querySelectorAll('.btn-create-request');
+    let recordingsCell = document.querySelectorAll('.portfolio-info');
+
     btnFilterRecordings.forEach( function(e) {
         e.addEventListener('click', function(event) {
-            let btnsManageRequest = document.querySelectorAll('.btn-view-request, ' +
-                                                              '.btn-delete-request');
             for (const btn of btnsManageRequest) {
                 btn.classList.add('btn-request-hidden');
                 let newHref = '/' + btn.getAttribute('href').split('/')[1] + '/'
@@ -14,12 +18,9 @@
         });
     });
 
-    let btnFilterRequest = document.querySelectorAll('.filter-request');
     btnFilterRequest.forEach( function(e) {
         e.addEventListener('click', function(event) {
             let request_id = event.target.getAttribute('data-filter').replace('.filter-request', '');
-            let btnsManageRequest = document.querySelectorAll('.btn-view-request, ' +
-                                                              '.btn-delete-request');
             for (const btn of btnsManageRequest) {
                 btn.classList.remove('btn-request-hidden');
                 let newHref = '/' + btn.getAttribute('href').split('/')[1] + '/' + request_id + '/'
@@ -27,14 +28,29 @@
             }
         });
     });
-    
-    let recordingsCell = document.querySelectorAll('.portfolio-info');
+
     recordingsCell.forEach( function(e) {
         let btnRecordingSelect = e.querySelector('.portfolio-links .bx-pointer');
         btnRecordingSelect.addEventListener('click', function(event) {
             let isSelected = (e.style.opacity === '1');
             e.style.opacity = isSelected ? '' : '1';
             e.style.background = isSelected ? '' : 'rgba(36, 135, 206, 0.6)';
+
+            if (!isSelected)
+                btnCreateRequest.classList.remove('btn-request-hidden');
+            else {
+                let createRequestIsEnabled = false;
+                for (const cell of recordingsCell) {
+                    if (cell.style.opacity === '1') {
+                        createRequestIsEnabled = true;
+                        break;
+                    }
+                }
+                if (createRequestIsEnabled)
+                    btnCreateRequest.classList.remove('btn-request-hidden');
+                else
+                    btnCreateRequest.classList.add('btn-request-hidden');
+            }
         });
     });
   })();
