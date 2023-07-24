94650a4def5cd78e089bdfab5beaa828e7c8e4db
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-23 20:39:30 -0400

    View account details button

---
+++ server/web/static/js/uploads-details.js
@@ -0,0 +1,27 @@
+(function () {
+    "use strict";
+  
+    let btnFilterSolutions = document.querySelectorAll('.filter-solutions');
+    let btnFilterSolution = document.querySelectorAll('.filter-solution');
+    let btnViewAccount = document.querySelector('.btn-view-account');
+
+    btnFilterSolutions.forEach( function(e) {
+        e.addEventListener('click', function(event) {
+            btnViewAccount.classList.add('btn-account-hidden');
+            btnViewAccount.setAttribute('account', '');
+            let newHref = '/' + btn.getAttribute('href').split('/')[1] + '/'
+            btn.setAttribute('href', newHref);
+        });
+    });
+
+    btnFilterSolution.forEach( function(e) {
+        e.addEventListener('click', function(event) {
+            let account_id = event.target.getAttribute('account');
+            btnViewAccount.classList.add('btn-account-hidden');
+            btnViewAccount.setAttribute('account', account_id);
+            let newHref = '/' + btn.getAttribute('href').split('/')[1] + '/' + account_id + '/'
+            btn.setAttribute('href', newHref);
+        });
+    });
+})();
+  
