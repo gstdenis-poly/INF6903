0a526b3d2903d335190762f06cace459ab6fb295
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-24 16:17:37 -0400

    Postgresql database V1

--- server/server/settings.py
+++ server/server/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../../db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gstdenis_db',
+        'USER': 'gstdenis',
+        'HOST': 'cedar-pgsql-vm',
+        'PORT': '5432',
         'OPTIONS': {
             'timeout': 20,
         },
