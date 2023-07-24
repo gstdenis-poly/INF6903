674d26e0fe6a6667fb955def0bc1432767b50d35
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2023-07-23 20:45:14 -0400

    Replace add by remove

--- server/web/static/js/uploads-details.js
+++ server/web/static/js/uploads-details.js
@@ -17,7 +17,7 @@
     btnFilterSolution.forEach( function(e) {
         e.addEventListener('click', function(event) {
             let account_id = event.target.getAttribute('account');
-            btnViewAccount.classList.add('btn-account-hidden');
+            btnViewAccount.classList.remove('btn-account-hidden');
             btnViewAccount.setAttribute('account', account_id);
             let newHref = '/' + btnViewAccount.getAttribute('href').split('/')[1] + '/' + account_id + '/'
             btnViewAccount.setAttribute('href', newHref);
